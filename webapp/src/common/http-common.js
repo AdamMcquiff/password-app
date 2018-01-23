17e9c3c983d7549c70779ac8fe25fe92eba0f25c
Adam McKenna <adam-mckenna@outlook.com>
2018-01-23 17:06:26 +0000

    API: JS lint; resolve CORS preflight error

--- webapp/src/common/http-common.js
+++ webapp/src/common/http-common.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: `http://localhost:8000/api/`
+  baseURL: `http://localhost:8000/api/`
 })
 
 export const httpAuth = axios.create({
-    baseURL: `http://localhost:8000/api/`,
-    headers: {
-        Authorization: 'Bearer ' + localStorage.getItem("token"),
-    }
+  baseURL: `http://localhost:8000/api/`,
+  headers: {
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  }
 })
 
 export const commonPasswordsApi = axios.create({
-    baseURL: 'https://moocher-io-common-passwords-v1.p.mashape.com/',
-    headers: {
-        'X-Mashape-Key': 'TO5gQ8nBo1mshnF8XKx1MRjWRk8Vp1d1539jsnvFibPUKRDX6s',
-        'Accept': 'text/plain',
-    }
+  baseURL: 'https://moocher-io-common-passwords-v1.p.mashape.com/',
+  headers: {
+    'X-Mashape-Key': 'TO5gQ8nBo1mshnF8XKx1MRjWRk8Vp1d1539jsnvFibPUKRDX6s',
+    'Accept': 'text/plain'
+  }
 })
